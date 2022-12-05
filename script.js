271b32569ad0a6370d04b39e08bfa3b862268340
Dylan Owens <90014103+DylanOwens-Dev@users.noreply.github.com>
2022-12-05 12:22:05 -0500

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let dice = document.querySelector('.dice');
+let players = document.querySelectorAll('.player');
+let player1 = document.querySelector('.player--0');
+let player2 = document.querySelector('.player--1');
+let active = document.querySelector('.player--active');
+let rollBtn = document.querySelector('.btn--roll');
+let holdBtn = document.querySelector('.btn--hold');
+let newBtn = document.querySelector('.btn--new');
+
+let num = Math.floor(Math.random() * 6) + 1;
+
+let init = function () {};
+
+rollBtn.addEventListener('click', function () {
+  dice.classList.remove('hidden');
+  dice.src = `dice-${num}`;
+});
